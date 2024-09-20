81605be238ccb0c662919c13a83fab25b852c60e
MursalinDevOps <mursalin1768@gmail.com>
2024-09-20 10:59:47 +0600

    Arithmetic Operations and pin validation added

---
+++ js/home.js
@@ -0,0 +1,47 @@
+// ADD MONEY FORM INIT
+const addMoneyForm = document.getElementById("add-money-form");
+const BtnAddMoney = document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    addMoneyForm.classList.remove("hidden");
+  });
+// WITHDRAW FORM INIT
+const withdrawForm = document.getElementById("withdraw-form");
+const btnWithdraw = document
+  .getElementById("btn-withdraw")
+  .addEventListener("click", function (event) {
+    withdrawForm.classList.remove("hidden");
+  });
+
+// MAIN OPERATIONS
+document.getElementById('add-money-btn').addEventListener('click', function(event){
+  event.preventDefault();
+  // INITIAL BALANCE
+  const availableBalanceStr = document.getElementById('available-balance').innerText;
+  const availableBalance = parseFloat(availableBalanceStr); // Converted string to number
+  // ADD MONEY AMOUNT
+  const addMoneyAmountStr = document.getElementById('add-money-amount').value;
+  const addMoneyAmount = parseFloat(addMoneyAmountStr);
+// PIN
+  const addMoneyPin = document.getElementById('add-money-pin').value;
+// VALIDATION
+if (addMoneyPin === '9332') {
+  // NEW/UPDATED BALANCE
+  const newBalance = availableBalance + addMoneyAmount;
+  availableBalanceStr.innerText = newBalance;
+} else {
+  alert('Your Pin is incorrect :( Try again.')
+}
+})
+
+//
+//       if (!isNaN(addAmount) && addAmount > 0) {
+//         const newBalance = availableBalance + addAmount;
+//         availableBalanceElement.innerText = newBalance.toFixed(2); // update the displayed balance
+//       } else {
+//         alert("Please enter a valid amount to add");
+//       }
+//     } else {
+//       alert("Wrong Number or Pin, Try again");
+//     }
+//   });
--- login.js
+++ login.js
@@ -1,14 +1,14 @@
 document.getElementById("login-btn").addEventListener("click", function (e) {
-    e.preventDefault(); // it prevents the site from reloading after click(reloading is default behavior)
-  
-  
-    const phoneNumber = document.getElementById("phone-number");
-    const pin = document.getElementById("pin-code");
-  
-    if (phoneNumber.value === "01953918566" && pin.value === "9332") {
-      alert("Valid User");
-    } 
-    else {
-      alert("Error! Invalid number or Password. Try again");
-    }});
-  
+  e.preventDefault(); // it prevents the site from reloading after click(reloading is default behavior)
+
+  const phoneNumber = document.getElementById("phone-number");
+  const pin = document.getElementById("pin-code");
+
+  if (phoneNumber.value === "01953918566" && pin.value === "9332") {
+    window.location.href = "/home.html"
+  } else {
+    alert("Error! Invalid number or Password. Try again");
+  }
+phoneNumber.value = '';
+pin.value = '';
+});
