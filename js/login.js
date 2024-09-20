666ea1f1a5bfeba6fc02dc3f064b0b80bfbff840
MursalinDevOps <mursalin1768@gmail.com>
2024-09-20 13:50:59 +0600

    pass and num changed

--- js/login.js
+++ js/login.js
@@ -4,8 +4,8 @@ document.getElementById("login-btn").addEventListener("click", function (e) {
   const phoneNumber = document.getElementById("phone-number");
   const pin = document.getElementById("pin-code");
 
-  if (phoneNumber.value === "01953918566" && pin.value === "1234") {
-    window.location.href = "./home.html"
+  if (phoneNumber.value === "01999999999" && pin.value === "1234") {
+    window.location.href = "/home.html"
   } else {
     alert("Error! Invalid number or Password. Try again");
   }
